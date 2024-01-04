d23912604723f13ffd920534f029a6511538cf52
Stimayk <51941742+Stimayk@users.noreply.github.com>
2024-01-04 12:52:14 +0400

    Create OfficialDSStatus.cs

---
+++ OfficialDSStatus.cs
@@ -0,0 +1,23 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+
+namespace OfficialDSStatus
+{
+    public class OfficialDSStatus : BasePlugin
+    {
+        public override string ModuleName => "OfficialDSStatus";
+        public override string ModuleVersion => "v1.0";
+
+        public override void Load(bool hotReload)
+        {
+            AddTimer(0.1f, () => SetIsValveDS());
+        }
+
+        public void SetIsValveDS()
+        {
+            CCSGameRules gamerules = Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules").First().GameRules!;
+            gamerules.IsValveDS = true;
+            Server.PrintToConsole($"IsValveDS - {gamerules.IsValveDS}");
+        }
+    }
+}
